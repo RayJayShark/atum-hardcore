1d69f4c0d4838a3b142d74797e104e05640bcf53
Joshua Ray <superdog1695@yahoo.com>
2025-09-04 21:38:15 -0400

    World is deleted when setting is set

--- src/main/java/me/voidxwalker/autoreset/mixin/hotkey/MinecraftClientMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/hotkey/MinecraftClientMixin.java
@@ -7,11 +7,19 @@ import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.*;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.session.Session;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.server.SaveLoader;
+import net.minecraft.server.integrated.IntegratedServer;
+import net.minecraft.world.level.storage.LevelStorage;
+import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.IOException;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
@@ -26,16 +34,27 @@ public abstract class MinecraftClientMixin {
     @Shadow
     public abstract void disconnectWithSavingScreen();
 
+    @Shadow
+    @Final
+    private LevelStorage levelStorage;
+
+    @Unique
+    private LevelStorage.Session levelStorageSession;
+
     @Inject(method = "startIntegratedServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/integrated/IntegratedServer;isLoading()Z", shift = At.Shift.AFTER))
-    private void resetPreview(CallbackInfo ci) {
+    private void resetPreview(LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, boolean newWorld, CallbackInfo ci) {
         if (Atum.isResetScheduled() && FabricLoader.getInstance().isModLoaded("worldpreview")) {
             this.clickButton(this.currentScreen, "menu.returnToMenu");
         }
+
+        levelStorageSession = session;
     }
 
     @Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;render(Z)V", shift = At.Shift.AFTER))
     private void executeReset(CallbackInfo ci) {
         while (Atum.shouldReset()) {
+            var shouldDeleteWorld = Atum.deleteWorld && this.isInSingleplayer();
+
             if (this.world != null) {
                 Screen gameMenuScreen = new GameMenuScreen(true);
                 gameMenuScreen.init((MinecraftClient) (Object) this, 0, 0);
@@ -46,9 +65,24 @@ public abstract class MinecraftClientMixin {
                     }
                 }
             }
+
+            if (shouldDeleteWorld) {
+                var levelName = levelStorageSession.getDirectoryName();
+                Atum.log(Level.INFO, "Deleting level: " + levelName);
+
+                try (LevelStorage.Session session = this.levelStorage.createSessionWithoutSymlinkCheck(levelName)) {
+                    session.deleteSessionLock();
+                } catch (IOException iOException) {
+                    Atum.log(Level.ERROR, "Failed to delete world: " + levelName);
+                }
+            }
+
             Atum.createNewWorld();
         }
     }
+
+    @Shadow
+    public abstract boolean isInSingleplayer();
 
     @Unique
     private boolean clickButton(Screen screen, String... translationKeys) {
