22ba433b93f23dd5a2da47751f887c56f4368eb5
Joshua Ray <superdog1695@yahoo.com>
2025-09-05 18:55:46 -0400

    Add reset button to death screen

--- src/main/java/me/voidxwalker/autoreset/mixin/DeathScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/DeathScreenMixin.java
@@ -2,12 +2,28 @@ package me.voidxwalker.autoreset.mixin;
 
 import me.voidxwalker.autoreset.Atum;
 import net.minecraft.client.gui.screen.DeathScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DeathScreen.class)
-public abstract class DeathScreenMixin {
+public abstract class DeathScreenMixin extends Screen {
+
+    protected DeathScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void moveAutoResetButton(CallbackInfo ci) {
+        this.addDrawableChild(ButtonWidget.builder(Atum.getTranslation("menu.reset", "Reset World"), (buttonWidget) -> {
+            Atum.isRunning = false;
+            Atum.scheduleReset();
+        }).dimensions(this.width / 2 - 50, this.height / 4 + 130, 100, 20).build());
+    }
+
     @Inject(method = "quitLevel", at = @At("HEAD"))
     private void stopResettingOnDeathQuit(CallbackInfo ci) {
         Atum.isRunning = false;
